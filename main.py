0bf03cfac8a9f4a25494ec0f1f1baf258838ce93
Jenna Rowan <jennagirl001@gmail.com>
2021-07-24 22:33:56 -0400

    +more refactor on labels and buttons, cleaner code

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ def gui_component_creation():
     # This function creates all the components in the GUI
 
     # Creates all the labels
-    title_label = create_labels("Title", 0, 0)
-    year_label = create_labels("Year", 1, 0)
-    author_label = create_labels("Author", 0, 2)
-    isbn_label = create_labels("ISBN", 1, 2)
+    title_label = create_label_or_button(Label, "Title", 0, 0)
+    year_label = create_label_or_button(Label, "Year", 1, 0)
+    author_label = create_label_or_button(Label, "Author", 0, 2)
+    isbn_label = create_label_or_button(Label, "ISBN", 1, 2)
 
     # Creates all the entry fields
     title_entry = create_entry_fields(title_entry_value, 0, 1)
@@ -28,19 +28,19 @@ def gui_component_creation():
     isbn_entry = create_entry_fields(isbn_entry_value, 1, 3)
 
     # Creates buttons for all the functions
-    view_all_button = create_buttons("View All", 2, 3)
-    search_entry_button = create_buttons("Search Entry", 3, 3)
-    add_entry_button = create_buttons("Add Entry", 4, 3)
-    update_selected_button = create_buttons("Update Selected", 5, 3)
-    delete_selected_button = create_buttons("Delete Selected", 6, 3)
-    close_button = create_buttons("Close", 7, 3)
+    view_all_button = create_label_or_button(Button, "View All", 2, 3)
+    search_entry_button = create_label_or_button(Button, "Search Entry", 3, 3)
+    add_entry_button = create_label_or_button(Button, "Add Entry", 4, 3)
+    update_selected_button = create_label_or_button(Button, "Update Selected", 5, 3)
+    delete_selected_button = create_label_or_button(Button, "Delete Selected", 6, 3)
+    close_button = create_label_or_button(Button, "Close", 7, 3)
 
-def create_labels(label_text, row_num, col_num):
+def create_label_or_button(function, component_text, row_num, col_num):
 
     # This function creates the labels for the user interface
-    created_label = Label(window, text = label_text)
-    created_label.grid(row = row_num, column = col_num)
-    return created_label
+    created_component = function(window, text = component_text)
+    created_component.grid(row = row_num, column = col_num)
+    return created_component
 
 def create_entry_fields(text_variable, row_num, col_num):
 
@@ -48,13 +48,6 @@ def create_entry_fields(text_variable, row_num, col_num):
     created_entry = Entry(window, textvariable = text_variable)
     created_entry.grid(row = row_num, column = col_num)
     return created_entry
-
-def create_buttons(button_text, row_num, col_num):
-
-    # This function creates all the buttons
-    created_button = Button(window, text = button_text)
-    created_button.grid(row = row_num, column = col_num)
-    return created_button
 
 # Sets window size
 window.geometry("500x500")
