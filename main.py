1ac52c4aa7142af9e606fbb3ffc90f94eb0502fa
Jenna Rowan <jennagirl001@gmail.com>
2021-07-24 22:18:25 -0400

    Refactored entry fields for cleaner code

--- main.py
+++ main.py
@@ -26,20 +26,12 @@ def create_labels():
     isbn_label = Label(window, text = "ISBN")
     isbn_label.grid(row = 1, column = 2)
 
-def create_entry_fields():
+def create_entry_fields(text_variable, row_num, col_num):
 
     # This function creates the text entry fields where users can enter data
-    title_entry = Entry(window, textvariable = title_entry_value)
-    title_entry.grid(row = 0, column = 1)
-
-    year_entry = Entry(window, textvariable = year_entry_value)
-    year_entry.grid(row = 1, column = 1)
-
-    author_entry = Entry(window, textvariable = author_entry_value)
-    author_entry.grid(row = 0, column = 3)
-
-    isbn_entry = Entry(window, textvariable = isbn_entry_value)
-    isbn_entry.grid(row = 1, column = 3)
+    created_entry = Entry(window, textvariable = text_variable)
+    created_entry.grid(row = row_num, column = col_num)
+    return created_entry
 
 def create_buttons(button_text, row_num, col_num):
 
@@ -52,6 +44,12 @@ def create_buttons(button_text, row_num, col_num):
 # Sets window size
 window.geometry("500x500")
 
+# Creates all the entry fields
+title_entry = create_entry_fields(title_entry_value, 0, 1)
+year_entry = create_entry_fields(year_entry_value, 1, 1)
+author_entry = create_entry_fields(author_entry_value, 0, 3)
+isbn_entry = create_entry_fields(isbn_entry_value, 1, 3)
+
 # Creates buttons for all the functions
 view_all_button = create_buttons("View All", 2, 3)
 search_entry_button = create_buttons("Search Entry", 3, 3)
@@ -62,7 +60,6 @@ close_button = create_buttons("Close", 7, 3)
 
 # Runs GUI creation functions
 create_labels()
-create_entry_fields()
 
 # Gives label columns a bit of padding for the sake of a cleaner UI
 window.columnconfigure(0, weight = 1)
