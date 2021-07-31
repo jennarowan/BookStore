afd2bd6c27e3b84ccd656e27b2f26e6a09a77c28
Jenna Rowan <jennagirl001@gmail.com>
2021-07-31 19:14:55 -0400

    Changed textbox to listbox, added lbox scrollbar

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-# This program builds a desktop program to hold book data for a fictional book store using Tkinter
+"""
+This program builds a desktop program to hold book data for a fictional book store using Tkinter
+
+Each book will have its Title, Author, Year of Publication, and ISBN stored
+
+A user can view the records, search for an entry, add/update/delete entries, and close the program
+"""
 
 from tkinter import *
 
 # Tkinter boilerplate to create gui
 window = Tk()
 
-# Housekeeping variable creation
+# Variables to be used later in creating text entry fields; created here to prevent scope errors
 title_entry_value = StringVar()
 year_entry_value = StringVar()
 author_entry_value = StringVar()
@@ -35,8 +41,12 @@ def gui_component_creation():
     delete_selected_button = create_label_or_button(Button, "Delete Selected", 6, 3)
     close_button = create_label_or_button(Button, "Close", 7, 3)
 
-    # Creates textbox to display info on all books
-    book_info_textbox = create_textbox(8, 40, 3, 5, 0, 3)
+    # Creates listbox to display info on all books
+    book_info_listbox = create_listbox(8, 40, 3, 5, 0, 2)
+
+    # Creates scrollbar for listbox and assigns it
+    book_info_scrollbar = create_scrollbar(3, 2)
+    assign_y_scrollbar(book_info_listbox, book_info_scrollbar)
 
 def create_label_or_button(function, component_text, row_num, col_num):
 
@@ -52,12 +62,25 @@ def create_entry_fields(text_variable, row_num, col_num):
     created_entry.grid(row = row_num, column = col_num)
     return created_entry
 
-def create_textbox(height_num, width_num, row_num, row_span_num, col_num, col_span_num):
+def create_listbox(height_num, width_num, row_num, row_span_num, col_num, col_span_num):
 
-    # This function creates a textbox to display information
-    created_textbox = Text(window, height = height_num, width = width_num)
-    created_textbox.grid(row = row_num, rowspan = row_span_num, column = col_num, columnspan = col_span_num)
-    return created_textbox
+    # This function creates a listbox to display information
+    created_listbox = Listbox(window, height = height_num, width = width_num)
+    created_listbox.grid(row = row_num, rowspan = row_span_num, column = col_num, columnspan = col_span_num)
+    return created_listbox
+
+def create_scrollbar(row_num, col_num):
+
+    # This functions creates scrollbars
+    created_scrollbar = Scrollbar(window)
+    created_scrollbar.grid(row = row_num, column = col_num)
+    return created_scrollbar
+
+def assign_y_scrollbar(component_to_assign_to, scrollbar_to_assign):
+
+    # This function attaches a scrollbar to another component (such as a listbox or textbox) so that the scrollbar operates the vertical axis of the component
+    component_to_assign_to.configure(yscrollcommand = scrollbar_to_assign.set)
+    scrollbar_to_assign.configure(command = component_to_assign_to.yview)
 
 # Displays title at the top of the window and taskbar, book icon in taskbar
 window.title("Book Store App")
